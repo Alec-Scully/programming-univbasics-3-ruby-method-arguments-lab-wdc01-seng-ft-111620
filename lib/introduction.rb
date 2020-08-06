6e98690fca7dcad0fd1d587c3e1b431da9e7b0f5
Alec Scully <scullyalec@gmail.com>
2020-08-06 23:06:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,7 @@ end
 
 introduction
 
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
@@ -16,9 +17,13 @@ end
 introduction_with_language("Alec", "Ruby")
 
 
+introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
 
+introduction_with_language_optional("Alec")
 
-
+introduction_with_language_optional("Alec", "Java")
 
 
 
